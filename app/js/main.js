acc205e517a61c71a71caa2533b6cc32860c58f8
esa-kateryna <esa_kateryna@ukr.net>
2019-05-21 15:59:38 +0300

    desktop done

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,13 @@
 $ (function(){
-    $('.slider').slick({
-        dots: true
+    $('.slider_content').slick({
+        dots: true,
+        arrows: false    
     });
 
+    $(".menu, .up").on("click","a", function(event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+        top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
 });
